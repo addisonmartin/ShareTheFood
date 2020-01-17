c4849359339181d1d522aeb2e4ce9269c4da97e8
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2020-01-16 22:49:45 -0500

    Added remaining configuration for test factories to work within RSpec tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 require 'pundit/matchers'
+require 'spec/support/factory_bot'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
