426602b059a84925ee8a11c89cc736f07bdbde1d
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2020-01-17 01:45:09 -0500

    Fixed security vulnerabilities in the Pages Controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,19 +4,13 @@ class PagesController < ApplicationController
   # Renders the page based based on the given page parameter,
   # or renders the 404 error page if the page parameter is invalid.
   def show
-    if valid_page?
-      render template: "pages/#{params[:page]}"
+    page = sanitize(params[:page]) # Sanitize the page parameter to remove any dangerous characters!
+    # Only render pages that are explicitly whitelisted.
+    if ['home'].include? page
+      render template: "pages/#{page}"
     else
       render file: 'public/404.html', status: :not_found
     end
-  end
-
-  private
-
-  # Returns true if the page parameter matches a file in app/views/pages/*.html.erb
-  def valid_page?
-    page_path = Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb")
-    File.exist?(page_path)
   end
 
 end
