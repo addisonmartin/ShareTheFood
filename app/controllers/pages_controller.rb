93a6dbbf8e0e2a7b8f446c1b348584e799e7c5c0
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2020-01-17 02:30:11 -0500

    Fixed bug where PagesController would not properly load the requested page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,9 @@ class PagesController < ApplicationController
   # Renders the page based based on the given page parameter,
   # or renders the 404 error page if the page parameter is invalid.
   def show
-    # Sanitize the page parameter to remove any dangerous characters!
-    page = sanitize(params[:page])
     # Only render pages that are explicitly whitelisted.
-    if ['home'].include? page
-      render template: "pages/#{page}"
+    if ['home'].include? params[:page]
+      render template: "pages/#{params[:page]}"
     else
       render file: 'public/404.html', status: :not_found
     end
