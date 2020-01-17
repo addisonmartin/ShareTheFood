abb14f59977566f225f5a3cde82e005687524390
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2020-01-17 14:38:14 -0500

    Enabled protection from cross-site request forgery attacks.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Enables protection against cross-site request forgery (CSRF) attacks.
+  protect_from_forgery with: :exception
 end
