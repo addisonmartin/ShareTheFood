a916f0974b19ccc98db1c2738907108587dbe941
Addison Martin <addisonejmartin@gmail.com>
2020-04-26 17:18:57 -0400

    Installed Bootstrap icons.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@
 module ApplicationHelper
   # Enables pagination of models in views.
   include Pagy::Frontend
+
+  # Used to load Bootstrap's SVG icons into HTML pages.
+  def icon(icon, options = {})
+    file = File.read("node_modules/bootstrap-icons/icons/#{icon}.svg")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css('svg')
+
+    # Add any of Bootstrap's classes, if provided.
+    svg['class'] += ' ' + options[:class] if options[:class].present?
+
+    doc.to_html.html_safe
+  end
 end
