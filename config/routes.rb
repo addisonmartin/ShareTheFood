a80bec7220853fefd81f7bb7bb86ed44a4124f3e
Addison Martin <addisonejmartin@gmail.com>
2020-04-26 13:19:06 -0400

    Created a controller to display static pages not backed by a database model. Created the about page, and set it as the application's current homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  # Sets up the routes for user authentication through Devise.
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Sets up the routes for static pages controlled by the PagesController.
+  controller :pages do
+    # sharethefood.net/about
+    get :about
+  end
+
+  # Sets the application's homepage to sharethefood.net/about
+  root 'pages#about'
 end
