3fbdbfaba3dd8942ec780712a7521199f386dbf4
Addison Martin <addisonejmartin@gmail.com>
2020-04-26 13:59:13 -0400

    Removed visits from donations, since multiple visits can happen per donation. Added database constraints to donation's columns.

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,7 +1,4 @@
 class Donation < ApplicationRecord
   # Configures Donations to belong to a User.
   belongs_to :user, inverse_of: :donations
-
-  # Configures Donations to keep track of User Visits, for analytics.
-  visitable :ahoy_visit
 end
--- db/migrate/20200426174442_create_donations.rb
+++ db/migrate/20200426174442_create_donations.rb
@@ -3,29 +3,29 @@ class CreateDonations < ActiveRecord::Migration[6.0]
     create_table :donations do |t|
       # Configures Donations to belong to a User.
       t.belongs_to :user, null: false, foreign_key: true
-      # Configures Donations to keep track of User visits, for analytics.
-      t.bigint :ahoy_visit_id
 
-      t.text :name
-      t.text :description
-      t.decimal :latitude
-      t.decimal :longitude
-      t.text :pickup_notes
-      t.datetime :available_until
-      t.boolean :is_perishable
-      t.boolean :requires_preparation
-      t.boolean :is_vegetarian
-      t.boolean :is_vegan
-      t.boolean :is_gluten_free
-      t.boolean :contains_peanuts
-      t.boolean :contains_tree_nuts
-      t.boolean :contains_dairy
-      t.boolean :contains_soy
-      t.boolean :contains_egg
-      t.boolean :contains_fish
-      t.boolean :contains_shellfish
+      t.text :name, null: false
+      t.text :description, default: ''
+      t.decimal :latitude, precision: 10, scale: 6, null: false
+      t.decimal :longitude, precision: 10, scale: 6, null: false
+      t.text :pickup_notes, null: false
+      t.datetime :available_until, null: false
+      t.boolean :is_perishable, default: false
+      t.boolean :requires_preparation, default: false
+      t.boolean :is_vegetarian, default: false
+      t.boolean :is_vegan, default: false
+      t.boolean :is_gluten_free, default: false
+      t.boolean :contains_peanuts, default: false
+      t.boolean :contains_tree_nuts, default: false
+      t.boolean :contains_dairy, default: false
+      t.boolean :contains_soy, default: false
+      t.boolean :contains_egg, default: false
+      t.boolean :contains_fish, default: false
+      t.boolean :contains_shellfish, default: false
+
+      # Used to enable soft deleting of Donations.
       t.datetime :discarded_at
-
+      # Creates the created_at/updated_at datetime columns.
       t.timestamps
     end
   end
