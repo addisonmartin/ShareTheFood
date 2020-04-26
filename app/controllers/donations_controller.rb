ef57020bd3c25b2d5aa85f87aa4c0a57d884b03c
Addison Martin <addisonejmartin@gmail.com>
2020-04-26 14:27:03 -0400

    Created a user authorization policy for donations, and configured donation's controller to check for authorization on all actions.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -40,24 +40,37 @@ class DonationsController < ApplicationController
   # GET /donations
   def index
     @donations = Donation.all
+
+    # Ensure the User has permission to perform this action.
+    authorize @donations
   end
 
   # GET /donations/1
   def show
+    # Ensure the User has permission to perform this action.
+    authorize @donation
   end
 
   # GET /donations/new
   def new
     @donation = Donation.new
+
+    # Ensure the User has permission to perform this action.
+    authorize @donation
   end
 
   # GET /donations/1/edit
   def edit
+    # Ensure the User has permission to perform this action.
+    authorize @donation
   end
 
   # POST /donations
   def create
     @donation = Donation.new(donation_params)
+
+    # Ensure the User has permission to perform this action.
+    authorize @donation
 
     if @donation.save
       redirect_to @donation, notice: 'Donation was successfully created.'
@@ -68,6 +81,9 @@ class DonationsController < ApplicationController
 
   # PATCH/PUT /donations/1
   def update
+    # Ensure the User has permission to perform this action.
+    authorize @donation
+
     if @donation.update(donation_params)
       redirect_to @donation, notice: 'Donation was successfully updated.'
     else
@@ -77,18 +93,40 @@ class DonationsController < ApplicationController
 
   # DELETE /donations/1
   def destroy
+    # Ensure the User has permission to perform this action.
+    authorize @donation
+
     @donation.destroy
     redirect_to donations_url, notice: 'Donation was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_donation
-      @donation = Donation.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def donation_params
-      params.require(:donation).permit(:user_id, :name, :description, :latitude, :longitude, :pickup_notes, :available_until, :is_perishable, :requires_preparation, :is_vegetarian, :is_vegan, :is_gluten_free, :contains_peanuts, :contains_tree_nuts, :contains_dairy, :contains_soy, :contains_egg, :contains_fish, :contains_shellfish, :discarded_at)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_donation
+    @donation = Donation.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through:
+  def donation_params
+    params.require(:donation).permit(:user_id,
+                                     :name,
+                                     :description,
+                                     :latitude,
+                                     :longitude,
+                                     :pickup_notes,
+                                     :available_until,
+                                     :is_perishable,
+                                     :requires_preparation,
+                                     :is_vegetarian,
+                                     :is_vegan,
+                                     :is_gluten_free,
+                                     :contains_peanuts,
+                                     :contains_tree_nuts,
+                                     :contains_dairy,
+                                     :contains_soy,
+                                     :contains_egg,
+                                     :contains_fish,
+                                     :contains_shellfish)
+  end
 end
---
+++ app/policies/donation_policy.rb
@@ -0,0 +1,36 @@
+class DonationPolicy < ApplicationPolicy
+  # Any User can view Donation's index.
+  def index?
+    true
+  end
+
+  # Any User can view a Donation.
+  def show?
+    true
+  end
+
+  # Only signed in Users can create donations.
+  def create?
+    user_signed_in?
+  end
+
+  # Only signed in Users can create donations.
+  def new?
+    create?
+  end
+
+  # Only the creator of a Donation, or an admin User, can edit it.
+  def update?
+    (user.id == record.id) or user.admin?
+  end
+
+  # Only the creator of a Donation, or an admin User, can edit it.
+  def edit?
+    update?
+  end
+
+  # Only the creator of a Donation, or an admin User, can delete it.
+  def destroy?
+    (user.id == record.id) or user.admin?
+  end
+end
