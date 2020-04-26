67aff7e8d5f181f9b9bd1424cc54c746b07b048c
Addison Martin <addisonejmartin@gmail.com>
2020-04-26 14:00:39 -0400

    Setup donations to track changes and be soft deleteable.

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,9 @@
 class Donation < ApplicationRecord
+  # Enables tracking and storing changes to Donations.
+  has_paper_trail
+  # Enables soft deleting Donations.
+  include Discard::Model
+
   # Configures Donations to belong to a User.
   belongs_to :user, inverse_of: :donations
 end
