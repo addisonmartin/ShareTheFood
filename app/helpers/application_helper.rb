eec0cb4ff13edf6e90b91d2f3c6072e29345afbe
Addison Martin <addisonejmartin@gmail.com>
2020-04-26 17:39:06 -0400

    Added user links to the navigation bar dependant if the user is signed in or not.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,37 @@ module ApplicationHelper
 
     doc.to_html.html_safe
   end
+
+  # Used from the navigation bar, to display sign up/in links for users not signed in, and edit profile/log out links to signed in users.
+  def user_links_for_navigation_bar
+    content_tag(:div, class: 'collapse navbar-collapse order-2') do
+      content_tag(:ul, class: 'navbar-nav ml-auto') do
+        # User is not signed in.
+        if current_user.nil?
+          concat(content_tag(:li, class: 'nav-item active') do
+            link_to new_user_session_path, class: 'nav-link' do
+              icon('box-arrow-in-left') + ' Log In'
+            end
+          end)
+          concat(content_tag(:li, class: 'nav-item active') do
+            link_to new_user_registration_path, class: 'nav-link' do
+              icon('person-plus') + ' Sign Up'
+            end
+          end)
+        # User is signed in.
+        else
+          concat(content_tag(:li, class: 'nav-item active') do
+            link_to edit_user_registration_path, class: 'nav-link' do
+              icon('gear') + ' Edit Profile'
+            end
+          end)
+          concat(content_tag(:li, class: 'nav-item active') do
+            link_to destroy_user_session_path, method: :delete, class: 'nav-link' do
+              icon('box-arrow-in-right') + ' Logout'
+            end
+          end)
+        end
+      end
+    end
+  end
 end
