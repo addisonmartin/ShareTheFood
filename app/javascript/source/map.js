ea2adff8dc89b98e48f3c9e266c55c2e06893bc5
Addison Martin <addisonejmartin@gmail.com>
2020-05-02 16:09:08 -0400

    Removed javascript package leaflet-providers, because it wasn't being used. Working on having the map display properly and nicely.

--- app/javascript/source/map.js
+++ app/javascript/source/map.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function() {
     var map = L.map('donationmap').setView([51.505, -0.09], 13);
-    var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
+    var map_layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 });
