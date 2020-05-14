c743d3533ef3db791e672f33bfcaa5679c3ca586
Addison Martin <addisonejmartin@gmail.com>
2020-05-14 07:53:15 -0400

    Removed duplicate option for action mailer in production environment config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'sharethefood.net' }
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: 'utf-8'
 
   # Configures emails to be sent by SendGrid.
