e031938ceaeae2308a9c9c5e9c5df236413ef1b7
Addison Martin <addisonejmartin@gmail.com>
2020-05-13 20:39:36 -0400

    Configured Rail's action mailer to send emails with Sendgrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,18 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'www.sharethefood.net' }
+  config.action_mailer.default_url_options = { host: 'sharethefood.net' }
+
+  # Configures emails to be sent by SendGrid.
+  ActionMailer::Base.smtp_settings = {
+    :user_name => Rails.application.credentials.dig(:sendgird, :username),
+    :password => Rails.application.credentials.dig(:sendgird, :password),
+    :domain => 'sharethefood.net',
+    :address => 'smtp.sendgrid.net',
+    :port => 465,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
