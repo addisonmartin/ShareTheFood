19ac7903ad1c1c02da2f848df9db6cfb2b70679a
addisonmartin <addisonejmartin@gmail.com>
2020-07-19 16:00:55 -0400

    Setup GET donations/:id to use conditional GET.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -106,14 +106,20 @@ class DonationsController < ApplicationController
     # Ensure the User has permission to perform this action.
     authorize @donation
 
-    # Pass some of the Donation's attributes to Javascript for use within the map.
-    gon.donation_latitude = @donation.latitude
-    gon.donation_longitude = @donation.longitude
-    gon.donation_name = @donation.name
-    gon.donation_pickup_notes = @donation.pickup_notes
+    # Check to see if the Donation has changed since the last time the user requested it,
+    # and if it hasn't changed just return a not modified status to the user's browser.
+    if stale?(@donation)
+      respond_to do |wants|
+        # Pass some of the Donation's attributes to Javascript for use within the map.
+        gon.donation_latitude = @donation.latitude
+        gon.donation_longitude = @donation.longitude
+        gon.donation_name = @donation.name
+        gon.donation_pickup_notes = @donation.pickup_notes
 
-    # Decorate the Donation so its decorator methods can be used.
-    @donation = @donation.decorate
+        # Decorate the Donation so its decorator methods can be used.
+        @donation = @donation.decorate
+      end
+    end
   end
 
   # GET /donations/new
