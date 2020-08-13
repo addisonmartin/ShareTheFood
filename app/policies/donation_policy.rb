af7ec113926b38a720df348e6b82cf29713c114e
Addison Martin <addisonejmartin@gmail.com>
2020-08-13 19:12:16 -0400

    Configured donations and posts user authorization.

--- app/policies/donation_policy.rb
+++ app/policies/donation_policy.rb
@@ -1,3 +1,26 @@
 class DonationPolicy < ApplicationPolicy
+  # Everyone can view all donations.
+  def index?
+    true
+  end
 
+  # Everyone can view a donation.
+  def show?
+    true
+  end
+
+  # Only signed in users can create a donation.
+  def create?
+    not user.nil?
+  end
+
+  # Only the owner of a donation, or an admin user, can update a donation.
+  def update?
+    (record.user == user) or user.admin?
+  end
+
+  # Only the owner of a donation, or an admin user, can destroy a donation.
+  def destroy?
+    (record.user == user) or user.admin?
+  end
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,3 +1,26 @@
 class PostPolicy < ApplicationPolicy
+  # Everyone can view all posts.
+  def index?
+    true
+  end
 
+  # Everyone can view a post.
+  def show?
+    true
+  end
+
+  # Only admin users can create a post.
+  def create?
+    user.admin?
+  end
+
+  # Only admin users can update a post.
+  def update?
+    user.admin?
+  end
+
+  # Only admin users can destroy a post.
+  def destroy?
+    user.admin?
+  end
 end
