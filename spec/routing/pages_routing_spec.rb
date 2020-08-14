908847672bb3f07141a8078dd1761cf8070723fb
Addison Martin <addisonejmartin@gmail.com>
2020-08-14 00:36:40 -0400

    Implemented pages routing tests.

---
+++ spec/routing/pages_routing_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe PagesController, type: :routing do
+  describe "routing" do
+    it "routes to #about" do
+      expect(get: "/about").to route_to("pages#about")
+    end
+
+    it "routes to #about" do
+      expect(get: '/').to route_to('pages#about')
+    end
+  end
+
+  describe "routing with locale" do
+    it "routes to #about" do
+      expect(get: "/en/about").to route_to("pages#about", locale: 'en')
+    end
+
+    it "routes to #about" do
+      expect(get: '/en').to route_to('pages#about', locale: 'en')
+    end
+  end
+end
