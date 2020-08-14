1c9b89799000c603b0c399ae8d48710fccee1121
Addison Martin <addisonejmartin@gmail.com>
2020-08-13 20:17:03 -0400

    Added password length tests to user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,6 +49,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
+
+    it { should validate_length_of(:password).is_at_least(10) }
+    it { should validate_length_of(:password).is_at_most(256) }
   end
 
   describe 'database' do
