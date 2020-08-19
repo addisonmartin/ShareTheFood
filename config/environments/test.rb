e50496619e3e2382768a7263b2133d6de4bb6546
Addison Martin <addisonejmartin@gmail.com>
2020-08-18 22:53:35 -0400

    Setup test environment to use Redis caching.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,10 +22,11 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
-  # Show full error reports and disable caching.
+  # Show full error reports.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-  config.cache_store = :null_store
+
+  config.action_controller.perform_caching = true
+  config.cache_store = :redis_cache_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
