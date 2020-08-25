36b5d79537ce2ac44d6adc21f01da3557b3a4aec
Addison Martin <addisonejmartin@gmail.com>
2020-08-24 21:06:22 -0400

    Disabled caching in test environment.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local       = true
 
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   config.cache_store = :redis_cache_store
 
   # Raise exceptions instead of rendering exception templates.
