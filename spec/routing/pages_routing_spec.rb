5c4556eca8c2f48b911e55ff87f38d34b669936b
Addison Martin <addisonejmartin@gmail.com>
2020-08-28 23:31:04 -0400

    Updated descriptions of page's routing tests.

--- spec/routing/pages_routing_spec.rb
+++ spec/routing/pages_routing_spec.rb
@@ -4,21 +4,21 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :routing do
   describe 'routing' do
-    it 'routes to #about' do
+    it 'about path routes to #about' do
       expect(get: '/about').to route_to('pages#about')
     end
 
-    it 'routes to #about' do
+    it 'root path routes to #about' do
       expect(get: '/').to route_to('pages#about')
     end
   end
 
   describe 'routing with locale' do
-    it 'routes to #about' do
+    it 'about path with locale routes to #about' do
       expect(get: '/en/about').to route_to('pages#about', locale: 'en')
     end
 
-    it 'routes to #about' do
+    it 'root path with locale routes to #about' do
       expect(get: '/en').to route_to('pages#about', locale: 'en')
     end
   end
