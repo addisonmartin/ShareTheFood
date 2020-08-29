a9c8c047842ed76354300ad214d8f45d34a59fc1
Addison Martin <addisonejmartin@gmail.com>
2020-08-28 23:27:22 -0400

    Removed view tests as they are not correct.

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -3,26 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe 'posts/new', type: :view do
-  before do
-    assign(:post, Post.new(
-                    title: 'MyText',
-                    subtitle: 'MyText',
-                    published: false,
-                    user: nil
-                  ))
-  end
 
-  it 'renders new post form' do
-    render
-
-    assert_select 'form[action=?][method=?]', posts_path, 'post' do
-      assert_select 'textarea[name=?]', 'post[title]'
-
-      assert_select 'textarea[name=?]', 'post[subtitle]'
-
-      assert_select 'input[name=?]', 'post[published]'
-
-      assert_select 'input[name=?]', 'post[user_id]'
-    end
-  end
 end
--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -3,20 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe 'posts/show', type: :view do
-  before do
-    @post = assign(:post, Post.create!(
-                            title: 'MyText',
-                            subtitle: 'MyText',
-                            published: false,
-                            user: nil
-                          ))
-  end
 
-  it 'renders attributes in <p>' do
-    render
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(//)
-  end
 end
