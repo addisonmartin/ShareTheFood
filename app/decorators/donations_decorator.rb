cdf5d08bd95ebf4c992ecc2736a91a46fea8e2d6
Addison Martin <addisonejmartin@gmail.com>
2020-09-02 10:09:11 -0400

    Working on search within distance form on donation's index view.

--- app/decorators/donations_decorator.rb
+++ app/decorators/donations_decorator.rb
@@ -4,4 +4,8 @@ class DonationsDecorator < Draper::CollectionDecorator
   def search_range
     h.params.dig('search', 'within_distance') or 25
   end
+
+  def from_current_location
+    h.params.dig('search', 'from_current_location') == '1'
+  end
 end
