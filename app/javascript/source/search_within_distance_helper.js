869e7a99ce1d9ce85c63bbcfded3f4d4e3cf3aac
Addison Martin <addisonejmartin@gmail.com>
2020-09-03 22:21:56 -0400

    Updated donation's search within to work if the user selects to use their current location.

--- app/javascript/source/search_within_distance_helper.js
+++ app/javascript/source/search_within_distance_helper.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
     $('#search_from_current_location').change(function () {
         if (this.checked) {
             $('#search_from').prop('value', gon.latitude + ', ' + gon.longitude);
-            $('#search_from').prop('disabled', true);
+            $('#search_from').prop('readonly', true);
         }
         else {
-            $('#search_from').prop('disabled', false);
+            $('#search_from').prop('readonly', false);
         }
     });
 });
