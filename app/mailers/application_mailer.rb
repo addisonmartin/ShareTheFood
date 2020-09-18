45c8d607af5d614b9521feb2a5f19877b16116ad
Addison Martin <addisonejmartin@gmail.com>
2020-09-18 12:00:53 -0400

    Updated default mail from URL, and added class comment.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
+# The default mailer that all other mails inherit from.
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'contact@sharethefood.net'
   layout 'mailer'
 
   track open: true, click: true
